717d7a5fff30d84bbffbd72fd3f5f08322ddf04e
NarRakDo <kyunghun96@gmail.com>
2022-10-04 00:29:17 +0900

    d

---
+++ src/main/java/com/example/pmb/domain/web/auth/UserController.java
@@ -0,0 +1,5 @@
+package com.example.pmb.domain.web.user;
+
+public class UserController {
+
+}
---
+++ src/main/resources/security/jwt/JwtTokenProvider.java
@@ -0,0 +1,9 @@
+package com.example.pmb.global.config.security.jwt;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtTokenProvider {
+
+
+}
